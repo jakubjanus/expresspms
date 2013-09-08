076e62f8b9c23a6ef3a36fa4c120c8353e0a4e92
Jakub Janus <jakubjanus90@gmail.com>
2013-09-08 19:18:16 +0200

    REST api: issue controller annotated

--- routes/api/v1/issues.js
+++ routes/api/v1/issues.js
@@ -2,6 +2,7 @@ var utils          = require('../../../service/utilService');
 var projectService = require('../../../service/projectService');
 var issueService   = require('../../../service/issueService');
 
+// GET /projects/:projectId/issues
 exports.index = function(req, res){
 	var eventEmiter = utils.getDataEventEmiter();
 	issueService.findByProject(eventEmiter, req.body.projectId);
@@ -14,18 +15,22 @@ exports.index = function(req, res){
 	});
 };
 
+// POST /projects/:projectId/issues
 exports.create = function(req, res){
 
 };
 
+// GET /projects/:projectId/issues/:id
 exports.show = function(req, res){
 
 };
 
+// PUT /projects/:projectId/issues/:id
 exports.update = function(req, res){
 
 };
 
+// DELETE /projects/:projectId/issues/:id
 exports.destroy = function(req, res){
 
 };
